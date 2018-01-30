1d82e78d984d6a7f41bbefbf42479999798da3e5
adunn49 <apdunn49@gmail.com>
2018-01-31 11:41:35 +1300

    Reinstated previous code

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://test:test123@ds119078.mlab.com:19078/mysandbox'
-}
-mongoose.connect( db.localhost || db.mlab);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
