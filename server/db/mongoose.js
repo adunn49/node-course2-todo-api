e5140a4b4e4e3ec8ccfccb528da7d11120366192
adunn49 <apdunn49@gmail.com>
2018-01-31 11:37:12 +1300

    Fix for remote mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://test:test123@ds119078.mlab.com:19078/mysandbox'
+}
+mongoose.connect( db.localhost || db.mlab);
 
 module.exports = {mongoose};
