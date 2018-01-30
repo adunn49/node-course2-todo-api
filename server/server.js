e425182ca31fcca0dec63f816c138d4e171c3d0e
adunn49 <apdunn49@gmail.com>
2018-01-31 12:12:07 +1300

    Added Delete Todo

--- server/server.js
+++ server/server.js
@@ -43,6 +43,21 @@ app.get('/todos/:id', (req, res) => {
   }, (e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Todo id is invalid');
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send('Todo not found');
+    }
+    return res.send(todo);
+  }).catch((e) => {
+    return res.status(400).send('Could not delete todo');
+  })
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
